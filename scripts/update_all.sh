a49851fba18e17351d0e44feb21e53921a12717f
Austin Burk <apburk@gmail.com>
2022-12-19 15:05:14 -0500

    That should be fine for now.

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -5,5 +5,6 @@
 find *.zendesk.com -mindepth 1 -maxdepth 1 -type d | sed 's/\.zendesk.com\// /' | xargs --verbose -L1 ./scripts/update_articles.sh
 
 # Prettify all article JSON, for better diff
+echo "Prettifying all the articles..." >&2
 ./scripts/prettify_all.sh
 
