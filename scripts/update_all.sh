4f7b5b50d063fd9a17a7b6c31a89e8335d564fb1
Austin Burk <apburk@gmail.com>
2022-12-19 14:37:23 -0500

    Update all articles, and add GH action

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -3,3 +3,7 @@
 # Based on existing Zendesk subdomains and languages, try to fetch all un-fetched articles
 
 find *.zendesk.com -mindepth 1 -maxdepth 1 -type d | sed 's/\.zendesk.com\// /' | xargs --verbose -L1 ./scripts/update_articles.sh
+
+# Prettify all article JSON, for better diff
+./scripts/prettify_all.sh
+
